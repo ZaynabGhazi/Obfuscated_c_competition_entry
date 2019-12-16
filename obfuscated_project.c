cb18ba34f0a8e5aece5c4c83cbfeea462fb3eebc
Zaynab Ghazi <43979389+ZaynabGhazi@users.noreply.github.com>
2019-12-15 19:11:29 -0500

    Add files via upload

---
+++ obfuscated_project.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h> /* This is Zaynab's entry for a less selective OCCC - BMC'22*/
+#                       define                   wvQ1I char
+#                        define                 wwOI1 putchar(
+#                          define              vvO1I ' '
+#                           define            wv0I1 '_'
+#                      define                         vv01I  main(
+#                    define                             ww0I1 )
+#                   define                               vwOI1 int
+#                 define                                  vvOI1  +
+#               define                                     wwO1I ;
+#             define                                        ww01I for(
+#           define                                           vv0I1 =
+#         define                                              wvOI1 void
+#        define                                                wvQ11I ++
+#      define                                                     wvO1I {
+#    define                                                        vw0I1 }
+# define                                                             wv01I 'Y'
+#        define        /*..............*/wwQI1(vv,wv,vw)        ww01I;vv<wv;vv++){wwOI1 vw);}
+vwOI1     vv01I       wvOI1                        ww0I1      wvO1I            \
+vwOI1    vvQ1I[]      vv0I1 wvO1I 4,3,4,11,1,2,2,2,10,2      ,19,8,2,1,2,1,2,16\
+,1,2,1,1,3,1,1,1,3     ,1,11,1,2,1,1,2,1,6,1,1,1,2,1,1,     2,4,3,5,3,1,6,1,1,1\
+,1,2,1,2,1,3,4,1,3      ,2,2,1,2,1,1,1,1,3,1,3,2,1,2,1     ,1,1,2,2,1,1,2,1,3,2\
+,1,2,2,2,3,2,1,1,1,     2,1,1,1,1,5,1,3,1,4,1,2,1,1,2      ,1,2,1,1,1,1,1,2,1,2\
+,2,1,1,2,1,2,1,1,2,1     ,5,1,1,2,3,1,1,2,1,1,1,1,2,      2,2,1,2,1,1,2,1,1,1,2\
+,1,1,1,2,3,1,1,2,1,1,     1,2,1,2,1,8,1,2,1,2,1,1,1,     2,3,1,1,1,4,1,1,2,1,1,\
+3,1,3,1,2,1,3,2,1,2,1      ,2,1,2,2,2,1,1,1,4,2,1,2     ,1,2,1,3,2,1,2,1,1,1,2,\
+1,1,1,7,2,1,2,1,4,1,1,3    ,2,1,2,1,1,1,1,1,2,1,1,     14,1,1,20,1,1,10,1,1,6,1\
+,1,10,1,1,13,1,1,6,1,1,     1 vw0I1         wwO1I      wvQ1I wwwv[] vv0I1 wvO1I\
+'\\','/','\n','<','>','|'    vw0I1 wwO1I   wvQ1I      vwww[]vv0I1 wvO1I  wv0I1,\
+vvO1I ,wv0I1,  vvO1I,'.',     wv0I1,vvO1I,wv0I1,     vvO1I,'.',wv0I1,vvO1I,wv0I1,\
+vvO1I,wv0I1,'.',  wv0I1 ,      vvO1I,'.',wv0I1       ,2[wwwv] , 0[wwwv],  vvO1I \
+,0[wwwv],  vvO1I,  1[wwwv]     ,vvO1I,1[wwwv],      wv0I1 , 5[wwwv],     wv0I1,\
+ 5[wwwv], 1[wwwv],   vvO1I,     5[wwwv],wv0I1      ,vvO1I,wv0I1,  1[wwwv], vvO1I,\
+5[wwwv],wv0I1,vvO1I, wv0I1,      vvO1I,wv0I1     ,vvO1I, 0[wwwv],wv0I1, vvO1I ,\
+0[wwwv],vvO1I, 5[wwwv],wv0I1,     5[wwwv],       vvO1I, 5[wwwv],  vvO1I,   wv0I1,\
+vvO1I,  wv0I1, vvO1I,   wv0I1\
+,5[wwwv],   wv0I1,   5[wwwv],                   2[wwwv],  vvO1I,0[wwwv],  vvO1I,\
+wv01I,  vvO1I,1[wwwv],  vvO1I,                 wv0I1,vvO1I,0[wwwv],wv0I1,vvO1I,\
+wv0I1,  vvO1I,0[wwwv],vvO1I,                     0[wwwv],vvO1I,wv0I1,  0[wwwv],\
+wv0I1,vvO1I,0[wwwv],vvO1I,wv0I1,0[wwwv],1[wwwv],vvO1I,wv0I1,vvO1I,0[wwwv],vvO1I,\
+1[wwwv],vvO1I,5[wwwv],vvO1I,5[wwwv],vvO1I,5[wwwv],vvO1I,0[wwwv],5[wwwv],vvO1I,\
+5[wwwv],vvO1I,5[wwwv],vvO1I,wv0I1,1[wwwv],vvO1I,wv0I1,vvO1I,0[wwwv],vvO1I,0[wwwv],\
+1[wwwv],vvO1I,1[wwwv],vvO1I,5[wwwv],2[wwwv],vvO1I,0[wwwv],vvO1I,1[wwwv],0[wwwv],\
+vvO1I,wv0I1,1[wwwv],5[wwwv],vvO1I,5[wwwv],vvO1I,0[wwwv],1[wwwv],vvO1I,5[wwwv],\
+vvO1I,5[wwwv],vvO1I,1[wwwv],vvO1I,wv0I1,vvO1I,0[wwwv],5[wwwv],vvO1I,5[wwwv],vvO1I,\
+0[wwwv],vvO1I,wv0I1,1[wwwv],5[wwwv],vvO1I,wv01I,vvO1I,wv01I,vvO1I,0[wwwv],vvO1I,\
+5[wwwv],wv0I1,0[wwwv],vvO1I,5[wwwv],vvO1I,5[wwwv],wv0I1,0[wwwv],vvO1I,wv0I1\
+,1[wwwv],vvO1I,4[wwwv],vvO1I,3[wwwv],5[wwwv],vvO1I,5[wwwv],2[wwwv],vvO1I,0[wwwv],\
+wv0I1,1[wwwv],vvO1I,0[wwwv],wv0I1,vvO1I,4[wwwv],wv0I1,5[wwwv],vvO1I,5[wwwv],wv0I1,\
+5[wwwv],wv0I1,5[wwwv],vvO1I,'(',wv0I1,vvO1I,1[wwwv],wv0I1,5[wwwv],vvO1I,0[wwwv],\
+wv0I1,vvO1I,4[wwwv],wv0I1,5[wwwv],wv0I1,5[wwwv],vvO1I,1[wwwv],vvO1I,1[wwwv],wv0I1,\
+vvO1I,1[wwwv],wv0I1,5[wwwv],wv0I1,1[wwwv],0[wwwv],wv0I1,vvO1I,4[wwwv],wv0I1,1[wwwv],\
+0[wwwv],wv0I1,vvO1I,0[wwwv],wv0I1,5[wwwv],2[wwwv],vvO1I,0[wwwv],1[wwwv],vvO1I,\
+0[wwwv],1[wwwv],vvO1I,0[wwwv],1[wwwv],vvO1I,0[wwwv],1[wwwv],vvO1I,0[wwwv],1[wwwv],\
+vvO1I,0[wwwv],1[wwwv],vvO1I,0[wwwv],1[wwwv],2[wwwv] vw0I1 wwO1I ww01I vwOI1 vv vv0I1 \
+0 wwO1I vv < (sizeof(vwww)/sizeof(char)) wwO1I vv wvQ11I ww0I1 wvO1I vwOI1 ww vv0I1 0\
+ wwO1I wwQI1(ww,vv[vvQ1I],vv[vwww]) vw0I1 vw0I1
